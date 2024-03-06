4e8a47fdb12d797ea9112b5743fb8803ac3c80c7
PabloaaFS <161032386+PabloaaFS@users.noreply.github.com>
2024-03-06 09:41:12 +0100

    First commit

---
+++ cleancode1/src/MaxExercise.java
@@ -0,0 +1,17 @@
+/**
+ * Definir el método max que toma dos parametros de tipo entero, a y b, y devuelve un valor booleano.
+ * Comparar los valores de a y b utilizando una estructura de control if-else.
+ * Devolver el resultado de la comparacion, si a es mayor que el valor de b, el método devuelve true.
+ * Si el valor de a es igual al valor de b, el metodo devuelve false.
+ * Si ninguna de las condiciones anteriores se cumple (es decir, el valor de a es menor que el valor de b),
+ * el metodo tambien devuelve false.
+ * El método max es static para permitir su acceso directo sin necesidad de crear una instancia de la clase MaxExercise.
+ */
+
+public class MaxExercise {
+
+    public static boolean max(int a, int b) {
+        return a > b;
+    }
+
+}
